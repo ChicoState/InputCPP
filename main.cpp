f05fa5fbd99e91a4272e1b2d5b74de67b60c268b
Ryan Bonomo <rbonomo@mail.csuchico.edu>
2017-09-13 17:08:13 -0700

    fix errors in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 using namespace std;
 
 int main()
@@ -8,12 +10,13 @@ int main()
 
   do
   {
+    repeat = false;
     cout<<"Enter a whole number: \n";
     cin>>num1;
     cout<<"Enter another whole number: \n";
     cin>>num2;
 
-    if( num1 = num2 )
+    if( num1 == num2 )
     {
       cout<<"Numbers should be different!\n";
       repeat = true;
@@ -32,4 +35,3 @@ int main()
 
  return 0;
 }
-
